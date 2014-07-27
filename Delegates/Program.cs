7eead2684ef65643f74d0710480f7433fbf20e62
Anna <anna.voloshchuk@gmail.com>
2014-07-27 22:43:35 +0300

    1st commit

---
+++ Delegates/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+
+        public delegate int Calculate(int x, int y);
+
+        static void Main(string[] args)
+        {
+            Calculate sum = (int x, int y) => x + y;
+            Calculate difference = (int x, int y) => x - y;
+            Calculate product = (int x, int y) => x * y;
+            Calculate quotient = (int x, int y) => x / y;
+
+            Console.WriteLine("Please input first number");
+
+            var firstArgument = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please input second number");
+
+            var secondArgument = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please choose operation: add/sub/mul/div");
+
+            string operation = Console.ReadLine();
+
+
+            switch (operation)
+            {
+                case "add":
+                    {
+                        Console.WriteLine("Result: {0}", sum(firstArgument, secondArgument));
+                        break;
+                    }
+                case "sub":
+                    {
+                        Console.WriteLine("Result: {0}", difference(firstArgument, secondArgument));
+                        break;
+                    }
+                case "mul":
+                    {
+                        Console.WriteLine("Result: {0}", product(firstArgument, secondArgument));
+                        break;
+                    }
+                case "div":
+                    {
+                        Console.WriteLine("Result: {0}", quotient(firstArgument, secondArgument));
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Sorry, you've chosen unsupported operation");
+                        break;
+                    }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DelegatesArray/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesArray
+{
+    class Program
+    {
+        public delegate int RandomInt();
+        public delegate double AverageOfMethods(RandomInt[] array);
+
+        static void Main(string[] args)
+        {
+            RandomInt delegate1 = delegate()
+            {
+                Random number = new Random();
+                return number.Next(50);
+            };
+
+            RandomInt delegate2 = delegate() 
+            { 
+                Random number = new Random(); 
+                return number.Next(5, 100); 
+            };
+
+            RandomInt delegate3 = delegate()
+            {
+                Random number = new Random();
+                return number.Next(1,10);
+            };
+
+            
+            RandomInt[] delegatesArray = new RandomInt[] { delegate1, delegate2, delegate3 };
+
+            AverageOfMethods delegateForAverage = delegate (RandomInt[] inputArray) 
+            {
+               
+                int sum = 0;
+
+                foreach (RandomInt del in inputArray)
+                {
+                    sum += del();
+                }
+                return sum / inputArray.Length;
+            };
+
+            Console.WriteLine(delegateForAverage(delegatesArray));
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
